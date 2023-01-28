b7373a3e49ae1edaca50dad83772e348b5bcf7b3
Maria Zgheib <maria_zoughaib@live.com>
2023-01-28 13:38:17 +0200

    my first trial

---
+++ coe211/Everything.java
@@ -0,0 +1,38 @@
+
+
+	public class Everything{
+	public static void main(String[] args){
+char letter='a';
+char number='1';
+int val=18;
+final double PI=1.0;
+double agenextyear;
+agenextyear=val*PI +1;
+
+
+
+System.out.println("this is a backslash"+" "+"\\");
+
+System.out.println("\"my age is:\""+"\t"+ val +"\n"+"\"my age next year will be:\""+"\t"+agenextyear);
+
+System.out.println(1+2+" is different than: "+"1"+"2"+" is different than: "+(1+2));
+
+		System.out.println("this is "+1+ letter +"\t"+"\t"+(1+letter));
+		System.out.println(number+letter);
+		System.out.println(1+number);
+		System.out.println(1+letter);
+System.out.println("\n"+"SPACE");
+
+System.out.println('a'+'1');
+System.out.println('a'+'1'+"");
+System.out.println(""+'a'+'1');	
+
+System.out.println('a');
+System.out.println(letter);
+System.out.println(number);
+System.out.println('1');
+
+
+	}
+
+}
